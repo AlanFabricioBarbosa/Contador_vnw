4f0d1d9abd2215273aa707863824299906bb6fab
Alan Fabrício Barbosa da Silva <alanfabriciobarbosa1@gmail.com>
2023-09-25 23:50:35 -0300

    :sparkles: feat: Criado uma mensagem de aviso para o usuario

--- src/Components/Count/index.jsx
+++ src/Components/Count/index.jsx
@@ -3,22 +3,26 @@ import styles from './styles.module.css'
 
 export default function Count() {
     const [num, setNum] = useState(0);
+    const [message, setMessage] = useState('');
 
     const add = () => {
         if (num < 10) {
             setNum(num + 1);
-        }
+            setMessage('');
+        } else (setMessage('O contador atingiu seu valor máximo.'))
     };
 
     const sub = () => {
         if (num > 0) {
             setNum(num - 1);
-        }
+            setMessage('');
+        } else (setMessage('O contador atingiu seu valor mínimo.'))
     };
 
     return (
         <main className={styles.main_container}>
-            <p>{num}</p>
+            <p className={styles.numbers}>{num}</p>
+            <p>{message}</p>
             <section className={styles.button_container}>
                 <button onClick={add}> + </button>
                 <button onClick={sub}> - </button>
